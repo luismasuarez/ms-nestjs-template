242b37ebdec871f0b168b8fdf2e5521deb487b67
Luisma Suarez <luisma.suarezzz@gmail.com>
2026-02-19 14:35:30 -0500

    fix(prisma.service): correct import path for PrismaClient

--- src/shared/services/prisma.service.ts
+++ src/shared/services/prisma.service.ts
@@ -5,8 +5,8 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from 'generated/prisma/client';
 import { envs } from '../../shared/config/envs';
+import { PrismaClient } from '../prisma/generated/prisma/client';
 
 @Injectable()
 export class PrismaService
