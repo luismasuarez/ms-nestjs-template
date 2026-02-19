8906ba4dddc3dfe72b0b90c742ceadb7005ead1b
Luisma Suarez <luisma.suarezzz@gmail.com>
2026-02-19 18:18:29 -0500

    refactor(shared): remove ExampleHttpController and clean up imports

--- src/shared.module.ts
+++ src/shared.module.ts
@@ -1,16 +1,15 @@
 import { Global, Module } from '@nestjs/common';
+import { ExampleRpcController } from './example/example.rpc.controller';
 import { LoggingInterceptor } from './shared/lib/logging.interceptor';
 import { RabbitMQInterceptor } from './shared/lib/rabbitmq.interceptor';
+import { NativeRpcService } from './shared/services/native-rpc.service';
 import { PrismaService } from './shared/services/prisma.service';
 import { ResponseService } from './shared/services/response.service';
 import { RpcService } from './shared/services/rpc.service';
-import { NativeRpcService } from './shared/services/native-rpc.service';
-import { ExampleHttpController } from './example/example.http.controller';
-import { ExampleRpcController } from './example/example.rpc.controller';
 
 @Global()
 @Module({
-  controllers: [ExampleHttpController, ExampleRpcController],
+  controllers: [ExampleRpcController],
   providers: [
     PrismaService,
     ResponseService,
