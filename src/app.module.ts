7912945e40e8b548010135bc76d3027ed704b230
Luisma Suarez <luisma.suarezzz@gmail.com>
2026-02-19 21:16:29 -0500

    refactor(config): enhance environment validation with EnvVars class and update AppModule configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './shared.module';
+import { validateEnv } from './shared/config/envs';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SharedModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnv,
+    }),
+    SharedModule,
+  ],
 })
 export class AppModule { }
