70ac169e0b51f54d1be624a46293e433b7ad06ad
Luisma Suarez <luisma.suarezzz@gmail.com>
2026-02-19 23:46:01 -0500

    refactor(env): remove environment validation logic and related imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './shared.module';
-import { validateEnv } from './shared/config/envs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validate: validateEnv,
     }),
     SharedModule,
   ],
